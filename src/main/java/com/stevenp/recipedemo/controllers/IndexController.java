1b4f38961f03978bd10b53e273b99cb6194c4235
ptviet <ptviet@live.com>
2018-05-07 21:17:19 +1000

    Initial commit

---
+++ src/main/java/com/stevenp/recipedemo/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.stevenp.recipedemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index","/home"})
+    public String getIndexPage() {
+
+        return "index";
+    }
+
+}
